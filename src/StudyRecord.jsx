bd23a1ad9af673a57e367d2650cac1521f25977c
runa.sasaki <runnpappa929@gmail.com>
2026-03-20 15:45:10 +0900

    added delete funtion from supabase

--- src/StudyRecord.jsx
+++ src/StudyRecord.jsx
@@ -6,6 +6,7 @@ import { ShowRecords } from './components/ShowRecords';
 import {supabase} from "./supabaseClient";
 
 export function StudyRecord() {
+  const db_name = "study-record";
   const [title, setTitle] = useState("");
   const [time, setTime] = useState(0);
   const [records, setRecords] = useState([]);
@@ -16,44 +17,66 @@ export function StudyRecord() {
   const onChangeTitle = (event) => setTitle(event.target.value);
   const onChangeTime = (event) => setTime(parseInt(event.target.value, 10));
 
-  const onClickAdd = () => {
+  // supabaseから学習記録を取得
+  const getAllRecords = async () => {
+    const { data, error } = await supabase.from(db_name).select("*");
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    setRecords(data);
+    setTotalTime(data.reduce((accumulator, record) => accumulator + record.time, 0));
+    setIsLoading(false);
+  };
+
+  // 学習記録を表示
+  useEffect(() => {
+    getAllRecords();
+  }, []);
+
+  const onClickAdd = async () => {
     if (!title || !time) {
       setError("入力されていない項目があります");
       return;
     }
-    const newRecords = [...records, { title, time }];
-    setRecords(newRecords);
 
-    // 合計時間の算出
-    setTotalTime(newRecords.reduce((accumulator, record) => accumulator + record.time, 0));
+    try {
+      // 学習記録をsupabaseにインサート
+      const { error } = await supabase.from(db_name).insert({ title, time });
+      if (error) throw new Error(error);
 
-    // 値の初期化
-    setTitle("");
-    setTime(0);
-    setError("");
+      getAllRecords();
+      setError("");
+
+    } catch(error) {
+      console.error(`登録エラー：${error}`);
+      setError("登録に失敗しました。再度お試しください。");
+
+    } finally {
+      // 値の初期化
+      setTitle("");
+      setTime(0);
+    }
   };
 
-  useEffect(() => {
-    const getAllRecords = async () => {
-      // supabaseから学習記録を取得
-      const { data, error } = await supabase.from("study-record").select("*");
-      if (error) {
-        console.log(error);
-        return;
-      }
-
-      setRecords(data);
-      setIsLoading(false);
-    };
+  const onClickDelete = async (id) => {
+    const { error } = await supabase.from(db_name).delete().match({id});
+    if (error) {
+      setError("削除処理に失敗しました。再度お試しください。");
+      console.log(error);
+      return;
+    }
     getAllRecords();
-  }, []);
+    setError("");
+  };
 
   return (
       isLoading ? (<div>Loading...</div>) : (
       <>
         <h1>学習記録一覧</h1>
         <InputRecord title={title} time={time} onChangeTitle={onChangeTitle} onChangeTime={onChangeTime} />
-        <ShowRecords records={records} />
+        <ShowRecords records={records} onClickDelete={onClickDelete}/>
         <button onClick={onClickAdd}>登録</button>
         <p style={{ color: 'red' }}>{error}</p>
         <p>合計時間：{totalTime} / 1000 (h)</p>
